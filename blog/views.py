b217eb1516194ecae99361fadfa2aefc10b03b4f
Kent Aguilar <kentaguilar@gmail.com>
2018-07-02 00:21:15 +0800

    Bug fix on functionality

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ def post_comment(request, pk):
 
 def add_comment(request, pk):
     if request.method == "POST":
-        form = CommentForm(request.Post)
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.name = request.POST['name']
@@ -36,6 +36,6 @@ def add_comment(request, pk):
             comment.post_id = pk
             comment.save()
             return redirect('post_detail', pk=pk)
-        else:
-            form = CommentForm()
-        return render(request, 'blog/add_comment.html', {'form':form})
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form':form})
